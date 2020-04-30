f4bab7074f6535d072f9bd9a4b53bc95bdd437bc
Hongchao Liu <lbs.lhc@gmail.com>
2020-04-30 12:07:03 +0200

    make it possible to pass in service account token to PubSubConfig

--- google-cloud-pub-sub/src/main/scala/akka/stream/alpakka/googlecloud/pubsub/impl/GoogleSession.scala
+++ google-cloud-pub-sub/src/main/scala/akka/stream/alpakka/googlecloud/pubsub/impl/GoogleSession.scala
@@ -9,9 +9,13 @@ import akka.stream.Materializer
 import akka.stream.alpakka.googlecloud.pubsub.impl.GoogleTokenApi.AccessTokenExpiry
 
 import scala.concurrent.Future
+import scala.io.Source
 
 @InternalApi
-private[googlecloud] class GoogleSession(clientEmail: String, privateKey: String, tokenApi: GoogleTokenApi) {
+private[googlecloud] class GoogleSession(clientEmail: String,
+                                         privateKey: String,
+                                         tokenApi: GoogleTokenApi,
+                                         tokenPath: Option[String] = None) {
   protected var maybeAccessToken: Option[Future[AccessTokenExpiry]] = None
 
   private def getNewToken()(implicit materializer: Materializer): Future[AccessTokenExpiry] = {
@@ -24,16 +28,22 @@ private[googlecloud] class GoogleSession(clientEmail: String, privateKey: String
     g.expiresAt < (tokenApi.now + 60)
 
   def getToken()(implicit materializer: Materializer): Future[String] = {
-    import materializer.executionContext
-    maybeAccessToken
-      .getOrElse(getNewToken())
-      .flatMap { result =>
-        if (expiresSoon(result)) {
-          getNewToken()
-        } else {
-          Future.successful(result)
-        }
-      }
-      .map(_.accessToken)
+    tokenPath match {
+      case Some(path) =>
+        Future.successful(Source.fromFile(path).mkString)
+
+      case None =>
+        import materializer.executionContext
+        maybeAccessToken
+          .getOrElse(getNewToken())
+          .flatMap { result =>
+            if (expiresSoon(result)) {
+              getNewToken()
+            } else {
+              Future.successful(result)
+            }
+          }
+          .map(_.accessToken)
+    }
   }
 }
